9bf63364d4c063531d3067c5b9ad88e45f998128
dexkode4 <dexkode4@gmail.com>
2021-10-17 19:14:42 +0100

    update weather card

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,10 +1,4 @@
-import {
-  Flex,
-  Heading,
-  Image,
-  Text,
-  Tag,
-} from "@chakra-ui/react";
+import { Flex, Heading, Image, Text, Tag } from "@chakra-ui/react";
 import React from "react";
 import { IWeatherInfoSection } from "../types/interface";
 import { Temp } from "../types/TempEnums";
@@ -13,16 +7,20 @@ import { constructUrl } from "../utils";
 interface IWeatherCardProps {
   data: IWeatherInfoSection;
   handleSelect: () => void;
+  active: boolean;
 }
 
 const iconUrl = "http://openweathermap.org/img/wn/:icon@4x.png";
 
-export const WeatherCard = ({ data, handleSelect }: IWeatherCardProps) => {
-
+export const WeatherCard = ({
+  data,
+  handleSelect,
+  active,
+}: IWeatherCardProps) => {
   const calculateAverageTemp = (): number => {
-      const sum = data.data.reduce((acc, curr) => acc + curr.main.temp  ,0);
-      return Math.round(sum / data.data.length)
-  }
+    const sum = data.data.reduce((acc, curr) => acc + curr.main.temp, 0);
+    return Math.round(sum / data.data.length);
+  };
   return (
     <>
       <Flex
@@ -33,7 +31,7 @@ export const WeatherCard = ({ data, handleSelect }: IWeatherCardProps) => {
         borderRadius="md"
         p="2"
         my="5"
-        shadow="sm"
+        shadow={active ? "lg" : "sm"}
         onClick={handleSelect}
         // templateColumns="1fr"
         // justifyItems="center"
@@ -42,10 +40,12 @@ export const WeatherCard = ({ data, handleSelect }: IWeatherCardProps) => {
         alignItems="center"
         cursor="pointer"
         _hover={{
-          shadow: "lg"
+          shadow: "lg",
         }}
       >
-        <Heading fontSize="sm" color="white">{data.title}</Heading>
+        <Heading fontSize="sm" color="white">
+          {data.title}
+        </Heading>
         <Tag
           variant="subtle"
           colorScheme="blackAlpha"
--- src/components/WeatherCardContainer.tsx
+++ src/components/WeatherCardContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IconButton, Icon, Flex } from "@chakra-ui/react";
 import Carousel, { RenderArrowProps } from "react-elastic-carousel";
 import { IoChevronBackSharp, IoChevronForwardSharp } from "react-icons/io5";
@@ -34,8 +34,10 @@ interface IWeatherCardContainerProps {
 export const WeatherCardContainer = ({ data }: IWeatherCardContainerProps) => {
   const [weatherSegmentData, setWeatherSegmentData] =
     useState<Array<IBarchartData>>();
+  const [selectedCard, setSelectedCard] = useState<number>();
 
   const handleSelectWeatherCard = (index: number) => {
+    setSelectedCard(index)
     const weatherSegmentData: Array<IBarchartData> = data[index].data.map(
       (item) => ({
         time: dayjs(item.dt_txt).format("ha"),
@@ -44,7 +46,21 @@ export const WeatherCardContainer = ({ data }: IWeatherCardContainerProps) => {
     );
 
     setWeatherSegmentData(weatherSegmentData);
+
   };
+
+  useEffect(() => {
+    if(data){
+      const weatherSegmentData: Array<IBarchartData> = data[selectedCard ? selectedCard : 0]?.data?.map(
+        (item) => ({
+          time: dayjs(item.dt_txt).format("ha"),
+          temp: item.main.temp,
+        })
+      );
+  
+      setWeatherSegmentData(weatherSegmentData);
+    }
+  },[data])
 
   const renderArrow = (props: RenderArrowProps) => {
     return props.type === "PREV" ? (
@@ -83,6 +99,7 @@ export const WeatherCardContainer = ({ data }: IWeatherCardContainerProps) => {
             data={item}
             key={index}
             handleSelect={() => handleSelectWeatherCard(index)}
+            active={selectedCard === index}
           />
         ))}
       </Carousel>
