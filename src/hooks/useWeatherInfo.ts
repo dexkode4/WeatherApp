5f6cadb59d4690c17f5a6084a002728ec40de43d
dexkode4 <dexkode4@gmail.com>
2021-12-06 15:40:25 +0100

    fix background refetch bug

--- src/hooks/useWeatherInfo.ts
+++ src/hooks/useWeatherInfo.ts
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { getWeatherInfo } from "../api";
 import { ICoord } from "../types/interface";
 
 export const useWeatherInfo = (coord: ICoord | undefined, unit: string) => {
+
   return useQuery(
     ["weatherInfo", coord, unit],
     () => coord && getWeatherInfo(coord, unit),
     {
-      enabled: !!coord && !!unit,
+      enabled: !!coord,
+      refetchOnWindowFocus: false,
+      retry: 1
     }
   );
 };
